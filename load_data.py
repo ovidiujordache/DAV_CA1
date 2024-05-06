a937b4907ec8dc0d67113eac6d9ec815550d27c7
ovidiujordache <D00187472@student.dkit.ie>
2024-05-06 12:33:29 +0100

    update

--- load_data.py
+++ load_data.py
@@ -1,13 +1,14 @@
 from __init__ import *
 '''
 Problem Statement.
-When Economy is down[files:(live register, poverty rates)] the Crime[files:(crimes_in_ireland)] is UP.
-Also when Economy is down, Culture[files:(third_level_entrance,expenditure_on_culture)] is UP.
-When Economy is booming Culture and Art is down.As a vivid example, during the height of the Celtic Tiger in 2002, 
+When ECONOMY IS DOWN [files:(live register, poverty rates)]  CRIME  IS  UP [files:(crimes_in_ireland)].
+Also when ECONOMY IS DOWN, CULTURE IS UP [files:(third_level_entrance,expenditure_on_culture)].
+When ECONOMY IS BOOMING CULTURE AND ART IS DOWN. As a vivid example, during the height of the Celtic Tiger in 2002, 
 a controversial and widely debated monument, "The Spire of Dublin," was constructed. 
 Conversely, at the decline of the Celtic Tiger around 2008-2009, the Samuel Beckett Bridge was built,
  showcasing another masterpiece from Santiago Calatrava.
-
+Aiming to find an index to show the relationship between 
+Economy, Crime, Unemployment, Poverty, College Admissions, Culture
 
 
 '''
